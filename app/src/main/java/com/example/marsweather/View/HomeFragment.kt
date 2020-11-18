3845ef3d7907d172c3edb1e3853e822f1836fde0
Gift Moshobane <giftmoshobane@Lenister-se-Boek.local>
2020-11-18 20:56:53 +0200

    restructure according to MVVM

---
+++ app/src/main/java/com/example/marsweather/View/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.marsweather.View
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import com.example.marsweather.R
+import com.example.marsweather.ViewModel.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
