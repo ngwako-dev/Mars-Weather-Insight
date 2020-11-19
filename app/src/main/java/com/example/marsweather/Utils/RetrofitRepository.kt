2031db784eac3e864b00fc70f75f4178aa89fa46
Gift Moshobane <giftmoshobane@Lenister-se-Boek.local>
2020-11-19 22:31:04 +0200

    mvvm structure

--- app/src/main/java/com/example/marsweather/Utils/RetrofitRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.marsweather.Utils
-
-class RetrofitRepository {
-    var retrofitClient : RetrofitService = RetrofitClient().retrofitService
-    suspend fun getData() = retrofitClient.getMarsData()
-}
