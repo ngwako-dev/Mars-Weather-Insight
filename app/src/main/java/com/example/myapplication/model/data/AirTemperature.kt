892bad19a7335395735aa7e7a608d2ff033f5523
Gift Moshobane <giftmoshobane@Lenister-se-Boek.local>
2020-11-23 19:45:29 +0200

    fixing UI
    file:///Users/giftmoshobane/Desktop/Final-Mars/Mars-Weather-Insight/app/src/main/res/layout/forecast_items.xml

--- app/src/main/java/com/example/myapplication/model/data/AirTemperature.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.model.data
-
-data class AirTemperature (
-    var av: Double,
-    var ct: Int,
-    var mn: Double,
-    var mx: Double
-)
--- app/src/main/java/com/example/myapplication/model/data/WindSpeed.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.model.data
-
-data class WindSpeed(
-    var av: Double,
-    var ct: Int,
-    var mn: Double,
-    var mx: Double
-)
